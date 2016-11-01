2172ca94cc397f3974c782f3f29b8fa72290a5d1
Mitja Robas <mitja.robas@gmail.com>
2016-11-02 00:03:15 +0100

    Add files via upload

---
+++ PowerShell/07-CFG-UCS-VLAN.ps1
@@ -0,0 +1,16 @@
+﻿<#
+VLAN creation paramters
+  VLAN name
+  Multicast policy - dfault
+  Common/Global
+  VLAN ID
+  Sharing Type - None, Primary, Isolaed, Community
+#>
+
+#### load VLAN pool list
+$listVLAN=Import-Csv "CSV-VLAN.csv"
+$list=$listVLAN
+
+#Create VLANs
+foreach($item in $list)
+ {Get-UcsLanCloud | Add-UcsVlan -Id $item.VLAN -Name $item.Name}
